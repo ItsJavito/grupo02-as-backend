60546789966a957f774294b6a28e20af6845561e
Javier <javierenriqueos@gmail.com>
2023-11-26 15:45:27 -0500

    add docker files

--- app_usuarios/src/controllers/auth.controller.js
+++ app_usuarios/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const config = require('../config/config');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
